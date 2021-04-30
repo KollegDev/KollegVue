cbe1e52a1dfd4b76babe563d8cf9eb07962b0a2d
Vinyl Smyk <80162687+ScratchyDot@users.noreply.github.com>
2021-04-30 02:14:06 +0200

    Added example project page

--- pages/projects/taichi.vue
+++ pages/projects/taichi.vue
@@ -4,7 +4,7 @@
 
 <script>
 import Project from "~/components/Project"
-import mdFile from "/static/markdown/doge.md"
+import mdFile from "/static/markdown/taichi.md"
 
 export default {
 	components: {
