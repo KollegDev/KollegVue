fee9c56a471e7e8b8d440e28a3684836c62c1b86
Vinyl Smyk <80162687+ScratchyDot@users.noreply.github.com>
2021-06-16 20:07:19 +0200

    Fixed SSR related bug on subjects page

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,6 +25,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~plugins/vue-carousel', ssr: false }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -44,6 +45,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    vendor: ['vue-carousel']
   },
 
   tailwindcss: {
