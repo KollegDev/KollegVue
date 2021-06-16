22270871593f45f5abea3c6b50bbf51357dd43df
Nightingale <stefan.ehn@hotmail.com>
2021-06-16 17:04:30 +0200

    Fixed loading of image gallery.

--- mixins/aos.js
+++ mixins/aos.js
@@ -1,8 +1,11 @@
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 
+
 export default {
     mounted() {
-        AOS.init({ mirror: true, duration: 400, offset: 70, startEvent: 'DOMContentLoaded', })
+      AOS.init({ mirror: true, duration: 400, offset: 70, startEvent: 'DOMContentLoaded', })
+      setTimeout(function(){ AOS.refreshHard(); }, 500);
+      setInterval(function(){ AOS.refresh(); }, 5000);
     }
-}
+};
