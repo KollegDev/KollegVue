ba2fcb93b5ec336e3444a7b4fd9df3b37cce85e1
Scratchy Dot <scratchydot@gmail.com>
2021-06-23 20:18:01 +0200

    Added initiatives to women page

--- pages/women.vue
+++ pages/women.vue
@@ -3,7 +3,7 @@
 		<div class="gap-8 pl-12 pr-12 mt-10 lg:flex">
 			<div class="flex-col">
 				<img
-					src="https://cdn.pixabay.com/photo/2017/08/02/01/11/people-2569404_1280.jpg"
+					src="https://htl.moedling.at/kolleg/images/woman/Frauen_it.jpg"
 					class="rounded-xl w-96"
 					alt="Frau am Laptop"
 					title="Frauen in der IT"
@@ -16,25 +16,14 @@
 					Warum Frauen in der IT?
 				</div>
 				<p class="text-2xl">
-					Informationstechnologie (IT) ist ein sehr breites Berufsfeld, das zunehmend
-					an Bedeutung gewinnt. Praktisch alle Wirtschaftszweige sind durch die
-					Digitalisierung erfasst. Um hier erfolgreich zu sein, brauchen
-					Organisationen gut ausgebildete Spezialistinnen und Spezialisten. In dieser
-					Branche ist nicht nur das technologische Know-how von Bedeutung, sondern
-					auch methodische Kenntnisse. Dazu gehören unter anderem:
-					Organisationsfähigkeit, Kommunikation, Teamarbeit sowie Kreativität.
-					Natürlich können auch Frauen hier eine wichtige Rolle spielen. Sie bringen
-					die richtigen Voraussetzungen mit.
+					Informationstechnologie (IT) ist ein sehr breites Berufsfeld, das zunehmend an Bedeutung gewinnt. Praktisch alle Wirtschaftszweige sind durch die Digitalisierung erfasst. Um hier erfolgreich zu sein, brauchen Organisationen gut ausgebildete Spezialistinnen und Spezialisten. In dieser Branche ist nicht nur das technologische Know-how von Bedeutung, sondern auch methodische Kenntnisse. Dazu gehören unter anderem: Organisationsfähigkeit, Kommunikation, Teamarbeit sowie Kreativität. Natürlich können auch Frauen hier eine wichtige Rolle spielen. Sie bringen die richtigen Voraussetzungen mit. 
 				</p>
 				<p class="text-2xl">
-					Der Frauenanteil in der IT-Branche ist aktuell noch gering. Nur 15,6 % der
-					Fachkräfte in Österreich sind weiblich (Daten aus 2017). Doch immer mehr
-					Unternehmen erkennen die Vorteile der weiblichen Mitarbeiterinnen und
-					erklären sich bereit mehr Frauen in diesem Gebiet einzusetzen.
+					Der Frauenanteil in der IT-Branche ist aktuell noch gering. Nur 15,6 % der Fachkräfte in Österreich sind weiblich (Daten aus 2017). Doch immer mehr Unternehmen erkennen die Vorteile der weiblichen Mitarbeiterinnen und erklären sich bereit mehr Frauen in diesem Gebiet einzusetzen. 
 				</p>
 				<p class="text-2xl">
 					Dazu hat das österreichische AMS eine Initiative –
-					<a
+					<a class="underline text-light"
 						href="https://www.ams.at/arbeitsuchende/karenz-und-wiedereinstieg/so-unterstuetzen-wir-ihren-wiedereinstieg/fit-frauen-in-handwerk-und-technik"
 						>Frauen in Technik.
 					</a>
@@ -48,127 +37,69 @@
 					Warum das Kolleg für Informatik-Systemtechnik der HTL Mödling?
 				</div>
 				<p class="mb-8 text-2xl">
-					Das Kolleg für Informatik-Systemtechnik an der HTL Mödling kombiniert
-					erfolgreich Theorie und Praxis. Vermittelt werden Grundlagen und Konzepte
-					mit Schwerpunkt auf Programmierung, Datenbanken, Netzwerktechnik und
-					Medientechnik. Abgerundet wird das Programm durch Inhalte aus
-					Systemplanung, Projektmanagement, Wirtschaft und Recht. Die Teilnehmerinnen
-					und Teilnehmer können das erworbene Wissen in diversen fächerübergreifenden
-					Projektarbeiten anwenden.
+					Das Kolleg für Informatik-Systemtechnik an der HTL Mödling kombiniert erfolgreich Theorie und Praxis. Vermittelt werden Grundlagen und Konzepte mit Schwerpunkt auf Programmierung, Datenbanken, Netzwerktechnik und Medientechnik. Abgerundet wird das Programm durch Inhalte aus Systemplanung, Projektmanagement, Wirtschaft und Recht. Die Teilnehmerinnen und Teilnehmer können das erworbene Wissen in diversen fächerübergreifenden Projektarbeiten anwenden.
 				</p>
 			</div>
 			<div class="flex flex-col">
 				<img
-					src="https://cdn.pixabay.com/photo/2016/03/09/09/22/workplace-1245776_1280.jpg"
+					src="https://htl.moedling.at/kolleg/images/woman/KennenlernTreff.jpg"
 					class="w-96 rounded-xl"
 					alt="Bild von einem Team"
 					title="Kolleg für Informatik-Systemtechnik"
 				/>
+				<div class="pictures ml-4"></div>
 			</div>
 		</div>
-
-		<div class="pl-12 pr-12 lg:flex">
-			<div class="flex flex-col gap-3">
-				<p class="mt-5 text-2xl">
-					Das Kolleg fokussiert sich besonders auf Kenntnisse und Fähigkeiten rund um
-					Softwareentwicklung. Es bleibt aber nicht nur bei der Technik. Moderne
-					Konzepte aus dem Projektmanagement, wie z.B. Scrum, werden ausführlich im
-					Unterricht behandelt.
-				</p>
-				<p class="mb-5 text-2xl">Frauen gehören für uns einfach dazu.</p>
-				<p class="text-2xl">Noch nicht überzeugt?</p>
-				<p class="mb-5 text-2xl">
-					In den letzten Jahren blicken wir auf erfolgreiche Absolventinnen.
-				</p>
-				<div class="mt-10 text-3xl font-semibold text-light dark:text-dark">
-					Testimonials
-				</div>
-				<p class="text-2xl">
-					„Im Kolleg für Informatik kann man sich in den verschiedensten Bereichen
-					der IT eine gute Basis aneignen. Durch die große Vielfalt, ist für jeden
-					ein Fach dabei, dass einem besonders liegt bzw. interessiert. Auch wenn man
-					als Frau ein Defizit hat mit technischen Begriffen und Abläufen, so wird
-					einem in den ersten Monaten im Kolleg im Schnellverfahren der fehlende
-					Wissensstand vermittelt. Die Lehrer stehen interessierten SchülerInnen für
-					Fragen beiseite. Mit einer gewissen Begabung für Mathematik bzw. Technik
-					ist das Informatik-Kolleg auch für Frau geeignet. In der Berufswelt hat man
-					als Frau gute Chancen, da der Frauenanteil noch relativ gering ist.
-					Arbeitgeber haben gerne auch gemischte Teams, da die Sichtweise oder
-					Herangehensweise von Frauen oft mal anderes ist als bei den männlichen
-					Kollegen. Dadurch ergibt sich eine Vielfalt an Lösungsvarianten und eine
-					harmonische Ausgeglichenheit innerhalb der Teams.“
-				</p>
-				<p class="text-sm text-right">
-					Catrina K. Absolventin der HTL Mödling - Kolleg für Informatik -
-					Systemtechnik
-				</p>
-				<p class="pt-10 text-2xl">
-					• Weil die Kombination von social Skills mit technischem Fachwissen am
-					Arbeitsmarkt gefragter denn je ist<br />
-					• Weil eine kaufmännische Berufserfahrung gepaart mit Technikausbildung
-					viele Türen am Arbeitsmarkt öffnet<br />
-					• Weil die Ausbildung den Grundstein für zahlreiche Karrierechancen
-					bietet<br />
-					• Weil einfach nichts dagegenspricht!"
-				</p>
-				<p class="text-sm text-right">
-					Chris Absolventin der HTL Mödling - Kolleg für Informatik – Systemtechnik
-				</p>
-			</div>
-		</div>
-
-		<div class="pt-8 pl-12 pr-12 lg:flex pb-8ya">
-			<div class="flex flex-col">
-				<div class="mt-12 text-3xl font-semibold text-light dark:text-dark">
-					Initiativen
-				</div>
-				<div class="mt-8">
-					<img
-						src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849821_1280.jpg"
-						class="w-96 rounded-xl"
-						alt="Bild"
-						title="Initiative 1"
-					/>
-				</div>
-			</div>
-			<div class="flex flex-col">
-				<p class="ml-5 mr-5 text-2xl text-justify mt-28">
-					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-					eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-					voluptua.
-				</p>
-				<p class="mt-5 ml-5 mr-5 text-2xl text-justify">
-					At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-					gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-				</p>
-			</div>
-			<div class="flex flex-col mt-28">
-				<img
-					src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849821_1280.jpg"
-					class="w-96 rounded-xl"
-					alt="Bild"
-					title="Initiative 2"
-				/>
-			</div>
-		</div>
+	<h1 class="mt-4 text-4xl font-semibold text-center text-light dark:text-dark">
+			Initiativen
+	</h1>
+	<div class="flex flex-col flex-wrap justify-around border-t-4  sm:mr-10 border-dark sm:flex-row">
+		<subject-item
+				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+				fullHeader="Internationaler Mädchentag"
+				description="
+                Die HTL- Mödling setzt ein Zeichen für die Gleichberechtigung und leuchtet pink.
+				Internationaler Welt-Mädchentag am 11. Oktober | Plan (plan-international.at)"
+		/>
+		<subject-item
+				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+				fullHeader="Kennenlerntag"
+				description="
+                Jedes Jahr organisiert DI Silvia Niedrist (Genderbeauftragte der Abteilung) ein Treffen, um die neuen Frauen in der Abteilung kennenzulernen. In Gesprächen unter Frauen lassen sich in einer sonst männerdominierten Welt manche fachlichen, schulischen, aber auch persönlichen Probleme leichter lösen."
+		/>
+		<subject-item
+				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+				fullHeader="FIT-Studien-Infotag"
+				description="
+                Jedes Jahr bekommen die Kolleg – Schülerinnen Einblick in die MINT-Studienfächer an den österreichischen Universitäten und Fachhochschulen. Vor Ort erhalten sie aus erster Hand durch aktive Studentinnen aktuelle Informationen."
+		/>
+		<subject-item
+				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+				fullHeader="Schnupper-Tag"
+				description="
+                Machen Sie einen Termin beim Abteilungsvorstand aus und nehmen Sie im echten Unterricht teil."
+		/>
 	</div>
+</div>
 </template>
 
 <style lang="postcss">
 .pictures {
-	@apply h-16 p-2 shadow-md w-96 rounded-b-2xl;
+	@apply h-16 p-2 w-80 rounded-b-2xl;
 }
 
 .blury {
-	background: rgba(255, 255, 255, 0.1);
-	box-shadow: inset 0px 7px 12px rgba(0, 0, 0, 0.25);
-	backdrop-filter: blur(20px);
-	margin-top: -4px;
+	
 }
 </style>
 
 <script>
+import SubjectItem from "~/components/SubjectItem"
+
 export default {
 	transition: "slide-bottom",
+	components: {
+		SubjectItem,
+	},
 }
 </script>
