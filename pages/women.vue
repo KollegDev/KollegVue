d9e0ac19408e10e551a80af629cd0a93c01ddd06
Nightingale <stefan.ehn@hotmail.com>
2021-06-23 20:47:46 +0200

    Added testimonials to woman

--- pages/women.vue
+++ pages/women.vue
@@ -16,14 +16,26 @@
 					Warum Frauen in der IT?
 				</div>
 				<p class="text-2xl">
-					Informationstechnologie (IT) ist ein sehr breites Berufsfeld, das zunehmend an Bedeutung gewinnt. Praktisch alle Wirtschaftszweige sind durch die Digitalisierung erfasst. Um hier erfolgreich zu sein, brauchen Organisationen gut ausgebildete Spezialistinnen und Spezialisten. In dieser Branche ist nicht nur das technologische Know-how von Bedeutung, sondern auch methodische Kenntnisse. Dazu gehören unter anderem: Organisationsfähigkeit, Kommunikation, Teamarbeit sowie Kreativität. Natürlich können auch Frauen hier eine wichtige Rolle spielen. Sie bringen die richtigen Voraussetzungen mit. 
+					Informationstechnologie (IT) ist ein sehr breites Berufsfeld, das zunehmend
+					an Bedeutung gewinnt. Praktisch alle Wirtschaftszweige sind durch die
+					Digitalisierung erfasst. Um hier erfolgreich zu sein, brauchen
+					Organisationen gut ausgebildete Spezialistinnen und Spezialisten. In dieser
+					Branche ist nicht nur das technologische Know-how von Bedeutung, sondern
+					auch methodische Kenntnisse. Dazu gehören unter anderem:
+					Organisationsfähigkeit, Kommunikation, Teamarbeit sowie Kreativität.
+					Natürlich können auch Frauen hier eine wichtige Rolle spielen. Sie bringen
+					die richtigen Voraussetzungen mit.
 				</p>
 				<p class="text-2xl">
-					Der Frauenanteil in der IT-Branche ist aktuell noch gering. Nur 15,6 % der Fachkräfte in Österreich sind weiblich (Daten aus 2017). Doch immer mehr Unternehmen erkennen die Vorteile der weiblichen Mitarbeiterinnen und erklären sich bereit mehr Frauen in diesem Gebiet einzusetzen. 
+					Der Frauenanteil in der IT-Branche ist aktuell noch gering. Nur 15,6 % der
+					Fachkräfte in Österreich sind weiblich (Daten aus 2017). Doch immer mehr
+					Unternehmen erkennen die Vorteile der weiblichen Mitarbeiterinnen und
+					erklären sich bereit mehr Frauen in diesem Gebiet einzusetzen.
 				</p>
 				<p class="text-2xl">
 					Dazu hat das österreichische AMS eine Initiative –
-					<a class="underline text-light"
+					<a
+						class="underline text-light"
 						href="https://www.ams.at/arbeitsuchende/karenz-und-wiedereinstieg/so-unterstuetzen-wir-ihren-wiedereinstieg/fit-frauen-in-handwerk-und-technik"
 						>Frauen in Technik.
 					</a>
@@ -37,7 +49,13 @@
 					Warum das Kolleg für Informatik-Systemtechnik der HTL Mödling?
 				</div>
 				<p class="mb-8 text-2xl">
-					Das Kolleg für Informatik-Systemtechnik an der HTL Mödling kombiniert erfolgreich Theorie und Praxis. Vermittelt werden Grundlagen und Konzepte mit Schwerpunkt auf Programmierung, Datenbanken, Netzwerktechnik und Medientechnik. Abgerundet wird das Programm durch Inhalte aus Systemplanung, Projektmanagement, Wirtschaft und Recht. Die Teilnehmerinnen und Teilnehmer können das erworbene Wissen in diversen fächerübergreifenden Projektarbeiten anwenden.
+					Das Kolleg für Informatik-Systemtechnik an der HTL Mödling kombiniert
+					erfolgreich Theorie und Praxis. Vermittelt werden Grundlagen und Konzepte
+					mit Schwerpunkt auf Programmierung, Datenbanken, Netzwerktechnik und
+					Medientechnik. Abgerundet wird das Programm durch Inhalte aus
+					Systemplanung, Projektmanagement, Wirtschaft und Recht. Die Teilnehmerinnen
+					und Teilnehmer können das erworbene Wissen in diversen fächerübergreifenden
+					Projektarbeiten anwenden.
 				</p>
 			</div>
 			<div class="flex flex-col">
@@ -47,40 +65,110 @@
 					alt="Bild von einem Team"
 					title="Kolleg für Informatik-Systemtechnik"
 				/>
-				<div class="pictures ml-4"></div>
+				<div class="ml-4 pictures"></div>
 			</div>
 		</div>
-	<h1 class="mt-4 text-4xl font-semibold text-center text-light dark:text-dark">
+		<h1 class="mt-4 text-4xl font-semibold text-center text-light dark:text-dark">
 			Initiativen
-	</h1>
-	<div class="flex flex-col flex-wrap justify-around border-t-4  sm:mr-10 border-dark sm:flex-row">
-		<subject-item
-				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+		</h1>
+		<div
+			class="flex flex-col flex-wrap justify-around border-t-4  border-dark sm:flex-row"
+		>
+			<subject-item
+				image="https://htl.moedling.at/kolleg/images/woman/HTL_pink.jpg"
 				fullHeader="Internationaler Mädchentag"
 				description="
                 Die HTL- Mödling setzt ein Zeichen für die Gleichberechtigung und leuchtet pink.
 				Internationaler Welt-Mädchentag am 11. Oktober | Plan (plan-international.at)"
-		/>
-		<subject-item
-				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+			/>
+			<subject-item
+				image="https://htl.moedling.at/kolleg/images/woman/KennenlernTreff.jpg"
 				fullHeader="Kennenlerntag"
 				description="
                 Jedes Jahr organisiert DI Silvia Niedrist (Genderbeauftragte der Abteilung) ein Treffen, um die neuen Frauen in der Abteilung kennenzulernen. In Gesprächen unter Frauen lassen sich in einer sonst männerdominierten Welt manche fachlichen, schulischen, aber auch persönlichen Probleme leichter lösen."
-		/>
-		<subject-item
-				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+			/>
+			<subject-item
+				image="https://htl.moedling.at/kolleg/images/woman/FIT.jpg"
 				fullHeader="FIT-Studien-Infotag"
 				description="
                 Jedes Jahr bekommen die Kolleg – Schülerinnen Einblick in die MINT-Studienfächer an den österreichischen Universitäten und Fachhochschulen. Vor Ort erhalten sie aus erster Hand durch aktive Studentinnen aktuelle Informationen."
-		/>
-		<subject-item
-				image="https://htl.moedling.at/kolleg/images/subjects/code.jpg"
+			/>
+			<subject-item
+				image="https://htl.moedling.at/kolleg/images/woman/SchnupperTag.jpg"
 				fullHeader="Schnupper-Tag"
 				description="
                 Machen Sie einen Termin beim Abteilungsvorstand aus und nehmen Sie im echten Unterricht teil."
-		/>
+			/>
+		</div>
+		<h1
+			class="text-4xl font-semibold text-center border-b-4  mt-9 text-light dark:text-dark border-dark"
+		>
+			Testimonials
+		</h1>
+		<div class="pl-48 pr-48">
+			<div class="p-9">
+				<p class="text-xl italic sm:text-2xl">
+					„Im Kolleg für Informatik kann man sich in den verschiedensten Bereichen
+					der IT eine gute Basis aneignen. Durch die große Vielfalt, ist für jeden
+					ein Fach dabei, dass einem besonders liegt bzw. interessiert. Auch wenn man
+					als Frau ein Defizit hat mit technischen Begriffen und Abläufen, so wird
+					einem in den ersten Monaten im Kolleg im Schnellverfahren der fehlende
+					Wissensstand vermittelt. Die Lehrer stehen interessierten SchülerInnen für
+					Fragen beiseite. Mit einer gewissen Begabung für Mathematik bzw. Technik
+					ist das Informatik-Kolleg auch für Frau geeignet. In der Berufswelt hat man
+					als Frau gute Chancen, da der Frauenanteil noch relativ gering ist.
+					Arbeitgeber haben gerne auch gemischte Teams, da die Sichtweise oder
+					Herangehensweise von Frauen oft mal anderes ist als bei den männlichen
+					Kollegen. Dadurch ergibt sich eine Vielfalt an Lösungsvarianten und eine
+					harmonische Ausgeglichenheit innerhalb der Teams.“
+				</p>
+				<p class="text-right">
+					Catrina K, Absolventin der HTL Mödling - Kolleg für Informatik -
+					Systemtechnik
+				</p>
+			</div>
+			<div class="p-9">
+				<p class="text-xl italic sm:text-2xl">
+					• „Weil die Kombination von social Skills mit technischem Fachwissen am
+					Arbeitsmarkt gefragter denn je ist.<br />
+					• Weil eine kaufmännische Berufserfahrung gepaart mit Technikausbildung
+					viele Türen am Arbeitsmarkt öffnet.<br />
+					• Weil die Ausbildung den Grundstein für zahlreiche Karrierechancen
+					bietet.<br />
+					• Weil einfach nichts dagegenspricht!“
+				</p>
+				<p class="text-right">
+					Chris, Absolventin der HTL Mödling - Kolleg für Informatik - Systemtechnik
+				</p>
+			</div>
+			<div class="p-9">
+				<p class="text-xl italic sm:text-2xl">
+					„Das Informatik Kolleg an der HTL Mödling hat mir einen guten Einstieg in
+					die Berufswelt ermöglicht. Die Ausbildung vermittelt ein breitgefächertes
+					Grundwissen über die Bereiche Datenbanken, objektorientiertes Programmieren
+					und Webentwicklung.“
+				</p>
+				<p class="text-right">
+					Michelle K, Absolventin der HTL Mödling - Kolleg für Informatik -
+					Systemtechnik
+				</p>
+			</div>
+			<div class="p-9">
+				<p class="text-xl italic sm:text-2xl">
+					„Vier sehr spannende Semester im Kolleg für Informatik. Viel gelernt und
+					viel umgesetzt. Gute Mischung aus Theorie und Praxis. Engagierte
+					Lehrerinnen und Lehrer. Zeitgemäße Inhalte. Sehr gute Chancen für die
+					berufliche Entwicklung. Auch wenn es als Mutter von zwei Kindern keine
+					leichte Entscheidung war, eine viersemestrige Ausbildung anzufangen, bin
+					ich rückblickend froh darüber diesen Schritt gewagt zu haben.“
+				</p>
+				<p class="text-right">
+					Ivelina T, Absolventin der HTL Mödling - Kolleg für Informatik -
+					Systemtechnik
+				</p>
+			</div>
+		</div>
 	</div>
-</div>
 </template>
 
 <style lang="postcss">
@@ -89,7 +177,6 @@
 }
 
 .blury {
-	
 }
 </style>
 
