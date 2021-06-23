fb939bc7f7b6198f5a343673367d59b92a290e6a
Nightingale <stefan.ehn@hotmail.com>
2021-06-23 20:20:14 +0200

    Added more projects

---
+++ pages/projects/2minuten.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+export default {
+	beforeCreate() {
+		this.$router.replace({ name: "projectGallery" })
+		window.location.href = "https://www.youtube.com/watch?v=_xORuVK_8gs"
+	},
+}
+</script>
---
+++ pages/projects/canSwap.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+export default {
+	beforeCreate() {
+		this.$router.replace({ name: "projectGallery" })
+		window.location.href = "https://youtu.be/q7Z-9E-Re3o"
+	},
+}
+</script>
